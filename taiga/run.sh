2549e95eeb4f690edc6244d700b70f073efed337
Edmund Hood Highcock <edmund@greenbyte.com>
2023-02-10 14:31:48 +0100

    Launch taiga protected

--- taiga/run.sh
+++ taiga/run.sh
@@ -6,7 +6,10 @@ PYTHONUNBUFFERED=true DJANGO_SETTINGS_MODULE=settings.config /home/taiga/taiga-b
 # Start taiga async
 PYTHONUNBUFFERED=true DJANGO_SETTINGS_MODULE=settings.config /home/taiga/taiga-back/.venv/bin/celery -A taiga.celery worker -B --concurrency 4 -l INFO &
 
+# Start taiga protected (which serves user-uploaded media securely)
+PYTHONUNBUFFERED=true /home/taiga/taiga-protected/.venv/bin/gunicorn --workers 4 --timeout 60 --log-level=info --access-logfile - --bind 0.0.0.0:8003 server:app &
 
+# Start the events service which keeps each open webpage in sync
 cd /home/taiga/taiga-events
 npm run start:production &
 
