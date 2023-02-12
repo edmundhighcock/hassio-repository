9b9d7eac2c250f188546fb5ab7a18654087f9d2d
Edmund Hood Highcock <edmund@greenbyte.com>
2023-02-12 14:24:19 +0100

    Added extra vhost to rabbitmq

--- rabbitmq/CHANGELOG.md
+++ rabbitmq/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [3.11.7-p1] - 2023-02-12
+
+### Added
+
+- Extra vhost: `taiga_local` for running local taiga installation
+
 ## [3.11.7] - 2023-02-08
 
 ### Added
--- rabbitmq/start.sh
+++ rabbitmq/start.sh
@@ -5,4 +5,11 @@ echo Hello Rabbits
 
 export RABBITMQ_DEFAULT_PASS=$(cat /data/options.json | jq -r .password) 
 
-rabbitmq-server
+rabbitmq-server &
+
+rabbitmqctl wait -P $!
+
+rabbitmqctl add_vhost taiga_local
+rabbitmqctl set_permissions -p taiga_local taiga ".*" ".*" ".*"
+
+wait
